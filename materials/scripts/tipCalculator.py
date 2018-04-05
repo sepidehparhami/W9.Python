9e2ecb712fef760cff9659cb23853576140180c6
Sepideh Parhami <sparhami@g.ucla.edu>
2018-04-05 12:41:51 -0700

    add Day 2 notes

--- materials/scripts/tipCalculator.py
+++ materials/scripts/tipCalculator.py
@@ -1,15 +1,29 @@
 import sys
-
 print(sys.argv)
 
 # Calculates cost of meal with tax and tip
-meal = 44.50
+
+def calculateTip(meal,tip):
+    meal = float(meal)
+    tip = float(tip)
+    tax = 0.075
+
+    meal += meal * tax
+    total = meal + meal * tip
+
+    return total
+
+
+meal = float( sys.argv[1])
 tax = 0.075
-tip = 0.15
+tip = float( sys.argv[2] )
 
-meal = meal + meal * tax
-total = meal + meal * tip
+meal += meal * tax
+total1 = meal + meal * tip
+print("%.2f" % total1)
 
-print("%.2f" % total)
+
+total2 = calculateTip(sys.argv[1],sys.argv[2])
+print("%.2f" % total2)
 
 
